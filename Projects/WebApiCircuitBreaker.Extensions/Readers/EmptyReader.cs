5513c825951cf9bad7a8691b27fe68b3ba3efca8
Nick Bitounis <n.bitounis@pamediakopes.gr>
2017-01-18 22:40:54 +0200

    Solution to hold concrete implementations that will be required.

---
+++ Projects/WebApiCircuitBreaker.Extensions/Readers/EmptyReader.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using WebApiCircuitBreaker.Core;
+using WebApiCircuitBreaker.Core.Interfaces;
+
+namespace WebApiCircuitBreaker.Extensions.Readers
+{
+    public class EmptyReader : IRuleReader
+    {
+        public IList<ConfigRule> ReadConfigRules()
+        {
+            return new List<ConfigRule>();
+        }
+    }
+}
