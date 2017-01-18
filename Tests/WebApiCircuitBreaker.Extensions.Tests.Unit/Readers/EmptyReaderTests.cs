d29211f7340d1faf95f53d477a7f31f93a3ac598
Nick Bitounis <n.bitounis@pamediakopes.gr>
2017-01-18 23:46:50 +0200

    Adding a missing test for the empty reader.

---
+++ Tests/WebApiCircuitBreaker.Extensions.Tests.Unit/Readers/EmptyReaderTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using WebApiCircuitBreaker.Extensions.Readers;
+
+namespace WebApiCircuitBreaker.Extensions.Tests.Unit.Readers
+{
+    [TestFixture]
+    public class EmptyReaderTests
+    {
+        [Test]
+        public void EnsureEmptyReaderReturnsEmptyList()
+        {
+            var reader = new EmptyReader();
+            var list = reader.ReadConfigRules();
+
+            Assert.IsNotNull(list);
+            Assert.AreEqual(0, list.Count);
+        }
+    }
+}
